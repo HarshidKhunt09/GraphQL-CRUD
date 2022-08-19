5a2f58c9e98aa84f11d0169a6d5940ec0eb9b59a
Harshid Khunt <harshidkhunt09@gmail.com>
2022-08-19 15:50:49 +0530

    ADD CRUD

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { AddClientModel } from '../components/AddClientModel';
+import { AddProjectModel } from '../components/AddProjectModel';
+import Clients from '../components/Clients';
+import Projects from '../components/Projects';
+
+export default function Home() {
+  return (
+    <>
+      <div className='d-flex gap-3 mb-4'>
+        <AddClientModel />
+        <AddProjectModel />
+      </div>
+      <Projects />
+      <hr />
+      <Clients />
+    </>
+  );
+}
